e2acde87fcd95be0232b06174ad8fc597df7b9a3
Amir <mirooguitar44@gmail.com>
2021-11-10 06:47:43 +0200

    done with client

--- server.js
+++ server.js
@@ -1,21 +1,49 @@
 // Setup empty JS object to act as endpoint for all routes
 const projectData = {};
 
+//Global variables
+const PORT = 3000;
 // Require Express to run server and routes
 const express = require("express");
-//Require Body Parser
+
 const bodyParser = require("body-parser");
+const cors = require("cors");
 // Start up an instance of app
 const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-console.log("hey");
-// Setup Server
+/**
+ * Routes
+ */
+//Get Routes
+app.get("/weather-data", (req, res) => {
+    //Return data object to client
+    res.send(projectData);
+});
+//Post routes
+app.post("/weather-data", (req, res) => {
+    // Extract values we need from request body
+    const { temp, date, content } = req.body;
+    // Check if everything is provided correctly or bad request returned
+    if (!(temp && date && content))
+        return res.status(400).json({ status: "fail", message: "Not all required Data provided!" });
+    // Assign recived data to our data project object
+    projectData.temp = temp;
+    projectData.date = date;
+    projectData.content = content;
+
+    // Response to client that everything is okay
+    res.send({ status: "success", message: "data recived successfully" });
+});
+
+// Listen to incoming requests on our port
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,117 @@
 /* Global Variables */
+const API_KEY = "6bd87df1befba6507163c45fe1370fb7";
+// Note:Zip code should work only on US since we will not provide a country
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+// Form elements
+const generateBtn = document.getElementById("generate");
+const zipInput = document.getElementById("zip");
+const feelingsInput = document.getElementById("feelings");
+// Entry elements
+const entryDate = document.getElementById("date");
+const entryTemp = document.getElementById("temp");
+const entryContent = document.getElementById("content");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+/**
+ * @description Post to server endpoint
+ * @param {string} url
+ * @param {object} data
+ */
+const postToServer = async (url, data) => {
+    try {
+        await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+    }
+    catch (e) {
+        // For test purposes
+        console.log(e.message);
+        throw new Error(e.message);
+    }
+};
+
+/**
+ * @description Making Get request to server end point
+ * @returns {object} {date,temp,content}
+ */
+const getDataFromServer = async () => {
+    try {
+        const res = await fetch("/weather-data");
+        const data = await res.json();
+        return data;
+    } catch (e) {
+        throw new Error(e.message);
+    }
+};
+
+/**
+ * @description Fetch data from open weather api
+ * @param {string} zip
+ * @returns {string} temp
+ */
+const weatherApiData = async (zip) => {
+    try {
+        // Units parameter used to get degrees in celsius
+        const res = await fetch(`${API_URL}&zip=${zip}&units=metric`);
+        const parsedData = await res.json();
+        const { temp } = parsedData.main;
+        return temp;
+    } catch (e) {
+        throw new Error(e.message);
+    }
+
+};
+
+/**
+ * @description Render data on server endpoin to client
+ */
+const renderEntry = async () => {
+    try {
+        // Request server data
+        const data = await getDataFromServer();
+        // Asign data to HTML elemnts useing their ids
+        entryDate.innerHTML = data.date;
+        entryTemp.innerHTML = data.temp + " C";
+        entryContent.innerHTML = data.content;
+
+    } catch (e) {
+        throw new Error(e.message);
+    }
+};
+
+/**
+ * @description Handle generate action on client side
+ * @param {Event} e
+ */
+const generateHandler = async (e) => {
+    try {
+        e.preventDefault();
+        // Content of text area box
+        const content = feelingsInput.value;
+        // Zip should work only for us since other countries needs edit to url
+        const zip = zipInput.value;
+        // Note: Temp in metric
+        const temp = await weatherApiData(zip);
+        await postToServer("/weather-data", { temp, content, date: newDate });
+        await renderEntry();
+    } catch (e) {
+        console.log("something went wrong");
+    }
+
+};
+
+/**
+ * @description EFI Main function which we need to call it once in beging
+ */
+(async () => {
+    // Listen to generate click
+    generateBtn.addEventListener("click", generateHandler);
+
+})();
